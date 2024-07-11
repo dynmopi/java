465ddc29a22593d3fb38e914c4d7a1b2fc760b7f
danrley <maranhaodanrley@gmail.com>
2024-07-11 18:45:32 -0300

    hello-world

---
+++ src/main/java/com/project/Main.java
@@ -0,0 +1,12 @@
+package com.project;
+// java é caseSensitive também.
+// Main e main são diferentes.
+
+// a class, deve começar em letra maiúscula.
+public class Main {
+
+    // o método main é a primeira instrução que o programa irá compilar.
+    public static void main (String[] args) {
+        System.out.println("Olá mundo");
+    }
+}       
