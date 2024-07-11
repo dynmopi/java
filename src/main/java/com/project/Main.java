9c3932727eba34f2a1ad3124cbd9c49d3309d0d7
danrley <maranhaodanrley@gmail.com>
2024-07-11 19:08:21 -0300

    variables

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -2,11 +2,17 @@ package com.project;
 // java é caseSensitive também.
 // Main e main são diferentes.
 
-// a class, deve começar em letra maiúscula.
+// a class, deve começar em letra maiúscula. E o nome do arquivo, deve corresponder
+// ao nome da classe
 public class Main {
 
     // o método main é a primeira instrução que o programa irá compilar.
     public static void main (String[] args) {
+        // println, é o método para imprimir, assim como o print que não pula uma
+        // linha.
         System.out.println("Olá mundo");
-    }
-}       
+        System.out.print("Estou aprendendo o Java ");
+        System.out.print("println e print são diferentes.");
+
+    }// as chaves servem para marcar o início e o fim dos codigos.
+}
---
+++ src/main/java/com/project/Variables.java
@@ -0,0 +1,9 @@
+package com.project;
+
+public class Variables {
+    public static void main(String[] args){
+
+        String name = "Danrley";
+        System.out.println(name);
+    }
+}
