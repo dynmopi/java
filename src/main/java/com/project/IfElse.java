b24c77182a078ee02b99d4d48d6eee33d3d3a703
danrley <maranhaodanrley@gmail.com>
2024-07-14 16:34:46 -0300

    switch

--- src/main/java/com/project/IfElse.java
+++ src/main/java/com/project/IfElse.java
@@ -1,5 +1,7 @@
 package com.project;
 
+import java.util.Calendar;
+
 public class IfElse {
     public static void main(String[] args){
         // o java possui instruções de condicionais
@@ -40,5 +42,37 @@ public class IfElse {
         System.out.println(result);
 
         // switch
+        // ao invés de escrever muitos if e elses, podemos usar o switch
+
+        // diferentemente do if else, o switch seleciona de imediato qual condição
+        // o valor se encaixa, fazendo com que o tempo das condicionais seja
+        // otimizada.
+        int day = 7;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default: // se o default for impresso no final do switch, não será necessário um break.
+                System.out.println("What day this?");
+        }
+        // o break, serve para o algoritmo sair dos switches
     }
 }
