f51b8cfa6830d9c971b54d0f7a92f7aed3b5f159
danrley <maranhaodanrley@gmail.com>
2024-07-16 12:21:41 -0300

    arrays

--- src/main/java/com/project/Arrays.java
+++ src/main/java/com/project/Arrays.java
@@ -17,13 +17,13 @@ public class Arrays {
             System.out.println(num[i]);
         }
 
-        // usando o forEach
+        // usando o forEach, é um for que já implementa o .length
         String[] cores = {"vermelho", "azul", "amarelo"};
         for (String contador : cores) {
             System.out.println(contador);
         }
 
-//         exemplo prático
+//         exemplo prático. Cálculo de média
 
         int[] idades = {20, 22, 18, 35, 48, 26, 87, 70};
 
@@ -32,7 +32,7 @@ public class Arrays {
         int comprimento = idades.length;
         int menorIdade = idades[0]; // o index zero é para a padronização
 
-
+    // forEach
         for (int idade : idades) {
             soma += idade;
             if (menorIdade > idade){
@@ -46,7 +46,7 @@ public class Arrays {
         // multidimensional arrays
         // consiste num array de arrays
 
-        // é útil quand o queremos armazenar valores em linhas e colunas. Portanto, possui
+        // é útil quando queremos armazenar valores em linhas e colunas. Portanto, possui
         // forma de tabela.
 
         float[][] multiArrays = {{1.1f, 2.1f, 3.1f}, {2.1f, 2.2f, 2.3f}};
@@ -55,12 +55,36 @@ public class Arrays {
         // segundo index = elemento
 
         // [1] segundo array [1] segundo elemento
+        // em java, o index 0 é dito como o primeiro.
 
-        // alterando um elemento do multi
+        // alterando um elemento do multiArrays
         multiArrays[1][0] = 7;
         System.out.println(multiArrays[1][0]);
 
+        System.out.println(multiArrays.length);
+        // especifica a quantidade de arrays dentro do próprio array
+        //
+        System.out.println(multiArrays[0].length); // com a possibilidade de especificar
+        // o index, nesse caso é imprimido os elementos do array filho.
 
+        // reatribuindo o segundo array do primeiro elemento.
+        multiArrays[1][0] = 2.1f; // não se esqueça de tipar o número!
+
+        // usando o for através de multiArrays para imprimir todos os elementos
+        for (int i = 0; i < multiArrays.length; i++){ // adere o comprimento do array pai (conjunto)
+            for (int j = 0; j < multiArrays[i].length; j++){ // adere o comprimento do array filho (elementos)
+                System.out.println(multiArrays[i][j]);
+            }
+        }
+        System.out.println("usando o forEach");
+        // usando o forEach
+
+        for (float[] elementoDoArray : multiArrays){
+            for (float elementoDoArrayFilho: elementoDoArray){
+                System.out.println(elementoDoArrayFilho); // e assim imprime todos
+                // os elementos dos dois arrays pai.
+            }
+        }
 
     }
 
