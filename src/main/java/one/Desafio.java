4dbaf5cd78c88700fd4e40600b0c2bb3b54b595f
Danrley Maranhão <134754910+dynmopi@users.noreply.github.com>
2024-10-02 19:00:36 -0300

    one

--- src/main/java/one/Desafio.java
+++ src/main/java/one/Desafio.java
@@ -22,5 +22,29 @@ public class Desafio {
 
         String mensagem = letra + palavra;
         System.out.println(mensagem);
+
+        //4. Declare uma variável do tipo double precoProduto e uma variável do tipo int (quantidade). Calcule o valor total multiplicando o preço do produto pela quantidade e apresente o resultado em uma mensagem.
+
+        double precoProduto = 5.5;
+        int quantidade = 10;
+
+        System.out.println("O valor total a pagar é de: " + precoProduto*quantidade);
+
+        //5. Declare uma variável do tipo double valorEmDolares. Atribua um valor em dólares a essa variável. Considere que o valor de 1 dólar é equivalente a 4.94 reais. Realize a conversão do valor em dólares para reais e imprima o resultado formatado.
+
+        double valorEmDolares = 4.94;
+        double valorEmReais = 7255.5;
+
+        String resultado = String.format("%.2f", valorEmReais/valorEmDolares);
+        System.out.println("O valor de" + valorEmReais + " para o dolar é de: " + resultado);
+
+        //5. Declare uma variável do tipo double precoOriginal. Atribua um valor em reais a essa variável, representando o preço original de um produto. Em seguida, declare uma variável do tipo double percentualDesconto e atribua um valor percentual de desconto ao produto (por exemplo, 10 para 10%). Calcule o valor do desconto em reais, aplique-o ao preço original e imprima o novo preço com desconto.
+
+        float precoOriginal = 500;
+        float percentualDesconto = 20;
+
+        float precoDescontado = 500 - (500 * 20/100);
+        System.out.println("O preço do produto é de " + precoOriginal + " reais, com desconto de " + percentualDesconto + "% o valor a se pagar é de " + precoDescontado);
+
     }
 }
