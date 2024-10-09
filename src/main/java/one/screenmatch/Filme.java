b7983f410fb2b0fa27343f40fb2fa43593c328a2
Danrley Maranhão <134754910+dynmopi@users.noreply.github.com>
2024-10-08 21:10:44 -0300

    screenmatch

--- src/main/java/one/screenmatch/Filme.java
+++ src/main/java/one/screenmatch/Filme.java
@@ -1,21 +1,17 @@
 package one.screenmatch;
 
 public class Filme {
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
+    // Organizando o código
+    // Variaveis com os modifiers
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
-    int duracaoDeMinutos;
+    private int duracaoDeMinutos;
+    //
 
-    double getsomaDasAvaliacoes(){
-        return somaDasAvaliacoes;
-    }
-
-    int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
-    }
-
+    // Operacao do sistema
     void exibeFichaTecnica() {
         System.out.println("Filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
@@ -30,5 +26,46 @@ public class Filme {
 
     double adquireMedia() {
         return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+    //
+
+    // metodos getSetters (seguranca)
+    public void setNome(String nome) {
+        this.nome = nome; // o nome foi incluido atraves do setter
+    }
+
+
+
+    String getNome() {
+        return nome; // o nome pode ser imprimido também
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public int getAnoDeLancamento(){
+        return anoDeLancamento; // agora posso mostrar o ano de lancamento pelo get.
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public boolean getIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setDuracaoDeMinutos(int duracaoDeMinutos) {
+        this.duracaoDeMinutos = duracaoDeMinutos;
+    }
+
+    public int getDuracaoDeMinutos() {
+        return duracaoDeMinutos;
+    }
+
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
     }
 }
--- src/main/java/one/screenmatch/Principal.java
+++ src/main/java/one/screenmatch/Principal.java
@@ -5,19 +5,21 @@ public class Principal {
         Filme umFilme = new Filme();
 
         // Primeiro Filme
-        umFilme.nome = "poderoso chefão";
-        umFilme.anoDeLancamento = 2010;
-        umFilme.duracaoDeMinutos = 180;
-        umFilme.incluidoNoPlano = true;
+        umFilme.setNome("Poderoso chefão"); // note, enviou o nome de forma segura
+        umFilme.setAnoDeLancamento(2010);
+        umFilme.setDuracaoDeMinutos(180);
+        umFilme.setIncluidoNoPlano(true);
         //
 
         umFilme.exibeFichaTecnica();
         umFilme.avalia(2);
         umFilme.avalia(8);
 
+        System.out.println("O nome do filme é: " + umFilme.getNome()); // o nome é imprimido
         System.out.println("A média do filme é de: " + umFilme.adquireMedia());
         System.out.println("O total de avaliações é de: " + umFilme.getTotalDeAvaliacoes());
 
 
     }
 }
+
