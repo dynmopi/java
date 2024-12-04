f45985b0231f246b61a38164630e5829b7d7cc4e
Danrley Maranhão <134754910+dynmopi@users.noreply.github.com>
2024-12-04 18:39:09 -0300

    one

---
+++ src/main/java/one/screenmatchAlura/screenMatch/principal/PrincipalComBusca.java
@@ -0,0 +1,29 @@
+package one.screenmatchAlura.screenMatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException
+    {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=e3f4c7fd";
+
+        // requisicao do server para o java local.
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        // response é o resultado do envio de request
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
